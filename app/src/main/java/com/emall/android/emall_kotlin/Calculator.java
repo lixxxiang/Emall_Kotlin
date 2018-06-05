f8bbb098180e0cede433f053847d1eef5d498dc9
lixxiang <1548806494@qq.com>
2018-06-05 17:11:40 +0800

    first commit

---
+++ app/src/main/java/com/emall/android/emall_kotlin/Calculator.java
@@ -0,0 +1,19 @@
+package com.emall.android.emall_kotlin;
+
+public class Calculator {
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double substract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+}
---
+++ app/src/test/java/com/emall/android/emall_kotlin/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.emall.android.emall_kotlin;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void sum() {
+    }
+
+    @Test
+    public void substract() {
+    }
+
+    @Test
+    public void divide() {
+    }
+
+    @Test
+    public void multiply() {
+    }
+}
